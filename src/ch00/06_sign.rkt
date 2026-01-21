2539ef28c9ca69fb85ea68dc5492d2c5af9d65d1
Artem Agafonov <agart@mail.ru>
2026-01-21 22:30:35 +0300

    ch00.md. Add info.

---
+++ src/ch00/06_sign.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 06_sign) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (sign x)
+  (cond
+    [(> x 0) 1]
+    [(= x 0) 0]
+    [(< x 0) -1]))
+
+(sign 10)  ; 1
+(sign -5)  ; -1
+(sign 0)   ; 0
